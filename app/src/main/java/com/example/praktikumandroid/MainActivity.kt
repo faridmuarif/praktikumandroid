7f65e2e031fe4b9aa34d4628d5910c567412917a
faridmuarif <fmfarid30@gmail.com>
2021-10-29 15:58:30 +0800

    Praktikum Android Pertemuan 1

---
+++ app/src/main/java/com/example/praktikumandroid/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.praktikumandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView1 = findViewById<TextView>(R.id.textView1);
+
+        textView1.setOnClickListener(View.OnClickListener {
+            Toast.makeText( this,"hallo",Toast.LENGTH_SHORT).show()
+        })
+
+
+    }
+}
